b14c70ca877280a11b6e265212d95c0b276e616f
Nathan Holt <69161294+NathanHolt@users.noreply.github.com>
2022-12-01 18:45:13 -0500

    I made it pretty :)

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 
+//data for display
+const list = ["Ibrahim", "Peter", "Natty-Lite", "Nathan", "Liz", "Ace", "Kel", "Josh", "Peter Again", "Ibrahim's Ipad"]
+
 // grabbing the main element
 const main = document.getElementById("main-page")
 
+//creating a new element
+let header = document.createElement("h2")
+header.innerHTML = "Hello and Greetings to:"
+
+//attaching new element to main
+main.appendChild(header)
+
 for(let i = 0; i < 10; i++) {
-    //creating a new element
-    let header = document.createElement("h2")
-    header.innerHTML = "Hello"
-    //attaching new element to main
-    main.appendChild(header)
+    let message = document.createElement("div")
+    message.innerHTML ="<ul><li>" + list[i] + "</li></ul>";
+
+    main.appendChild(message)
 }
+
