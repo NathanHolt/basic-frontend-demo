83f4d8acccc087068b0d989859ac72e1ae3cc44f
Nathan Holt <69161294+NathanHolt@users.noreply.github.com>
2022-12-02 20:11:21 -0500

    Q&A fri dec 2 - style and flex

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-
-//data for display
-const list = ["Ibrahim", "Peter", "Natty-Lite", "Nathan", "Liz", "Ace", "Kel", "Josh", "Peter Again", "Ibrahim's Ipad"]
-
-// grabbing the main element
-const main = document.getElementById("main-page")
-
-//creating a new element
-let header = document.createElement("h2")
-header.innerHTML = "Hello and Greetings to:"
-
-//attaching new element to main
-main.appendChild(header)
-
 //way of declaring method
 const addNum = (num1, num2) => {
    return num1 + num2
@@ -22,13 +8,28 @@ function addNum2(num1, num2) {
     return num1 + num2
 }
 
+//data for display
+const list = ["Ibrahim", "Peter", "Natty-Lite", "Nathan", "Liz", "Ace", "Kel", "Josh", "Peter Again", "Ibrahim's Ipad"]
 
-let message = document.createElement("div")
+// grabbing the main element
+const main = document.getElementById("main-page")
+
+//creating a new element
+const header = document.createElement("h2")
+header.innerHTML = "Hello and Greetings to:"
+
+//attaching new element to main
+main.appendChild(header)
+
+const message = document.createElement("div")
+message.className = "list";
+//creating a "Unordered List"
 let names = "<ul>"
 
 for(let i = 0; i < 10; i++) {
+    //adding "List Items" to the "Unordered List"
     names += "<li>" + list[i] + "</li>";
-    console.log(addNum2(i, i))
+//    console.log(addNum2(i, i))
 }
 
 names += "</ul>"
