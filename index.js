8f79da84d7558f02887f351e0df57811dfd8f499
Nathan Holt <69161294+NathanHolt@users.noreply.github.com>
2022-12-27 17:46:28 -0500

    added david demo

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function addNum2(num1, num2) {
     return num1 + num2
 }
 
+
+
+
+
 //data for display
 const list = ["Ibrahim", "Peter", "Natty-Lite", "Nathan", "Liz", "Ace", "Kel", "Josh", "Peter Again", "Ibrahim's Ipad"]
 
@@ -35,4 +39,87 @@ for(let i = 0; i < 10; i++) {
 names += "</ul>"
 message.innerHTML = names;
 main.appendChild(message)
+
+let showBoolean = true
+
+const btn = document.getElementById("show")
+const unbtn = document.getElementById("unshow")
+const funPage = document.getElementById("funPage")
+
+show.addEventListener('click', (e) => {
+    if (showBoolean) {
+        funPage.style.display = "block";
+        btn.innerText = "Hide Page"
+        showBoolean = false
+    } else {
+        funPage.style.display = "none";
+        btn.innerText = "Show Page"
+        showBoolean = true
+    }
+})
+
+unbtn.addEventListener('click', (e) => {
+    funPage.style.display = "none";
+    btn.innerText = "Show Page"
+    showBoolean = true
+})
+
+
+// listening for any key press and logging it
+//const body = document.getElementById("body")
+//
+//body.addEventListener('keydown', (e) => {
+//    console.log(e)
+//})
+//
+//
+
+
+
+
+
+const information = {
+    name: "Peter",
+    size: "large",
+    age: 23,
+    items: [
+        "straw",
+        "hat",
+        "tv"
+    ]
+}
+
+
+const addButton = document.getElementById("add")
+const infoDiv = document.getElementById("info")
+
+addButton.addEventListener('click', (e) => {
+
+
+    console.log(information)
+
+//    let tempHTML = "<div>"
+//    tempHTML += `<h2>${information.name}</h2>`
+//    tempHTML += `<h3>Age: ${information.age}</h3>`
+//    tempHTML += "<ul>"
+//
+    for(let i = 0; i < information.items.length; i++) {
+        tempHTML += `<li>${information.items[i]}</li>`
+    }
+
+//    tempHTML += "</ul></div>"
+//
+//    infoDiv.innerHTML = tempHTML
+//    infoDiv.innerHTML += tempHTML
+})
+
+
+
+
+
+
+
+
+
+
 
